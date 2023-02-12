6626ee1f7bca6de41ee18b04b2d7c4eb7ac16ea7
Stereo528 <52116771+Stereo528@users.noreply.github.com>
2023-02-12 13:34:31 -0700

    remove Simple Notes, 1.0.1

--- src/main/java/io/github/stereo528/yach/Config/ModConfig.java
+++ src/main/java/io/github/stereo528/yach/Config/ModConfig.java
@@ -4,6 +4,5 @@ import eu.midnightdust.lib.config.MidnightConfig;
 
 public class ModConfig extends MidnightConfig {
 	@Entry public static boolean showOnHotbar = true;
-	@Entry public static boolean simpleNotesIntegration = false;
 	@Entry public static boolean hideInSpectator = false;
 }
