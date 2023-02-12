4071e661cc526523e57a8cf74ecc229dd5f34f5d
Stereo528 <52116771+Stereo528@users.noreply.github.com>
2023-02-12 02:17:25 -0700

    cleanup + remember survival/adventure exists

--- src/main/java/io/github/stereo528/yach/mixin/GuiMixin.java
+++ src/main/java/io/github/stereo528/yach/mixin/GuiMixin.java
@@ -4,6 +4,7 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import io.github.stereo528.yach.Config.ModConfig;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.level.GameType;
 import org.quiltmc.loader.api.minecraft.ClientOnly;
 import org.spongepowered.asm.mixin.Mixin;
@@ -24,21 +25,37 @@ public class GuiMixin {
 	public void renderCoordsOnHotbar(PoseStack poseStack, float timeDelta, CallbackInfo cir) {
 		if(!client.options.renderDebug) {
 			if (ModConfig.showOnHotbar) {
-				assert this.minecraft.gameMode != null;
-				if (this.minecraft.gameMode.getPlayerMode() == GameType.SPECTATOR && ModConfig.hideInSpectator) {
+				assert client.gameMode != null;
+				if(client.gameMode.getPlayerMode() == GameType.SPECTATOR && ModConfig.hideInSpectator) {
 					poseStack.pushPose();
 					poseStack.popPose();
 				}
 				else {
-					poseStack.pushPose();
-					assert client.player != null;
-					String x = "X: " + (int) client.player.getX();
-					String y = "Y: " + (int) client.player.getY();
-					String z = "Z: " + (int) client.player.getZ();
-					client.font.drawShadow(poseStack, x, (int) (((this.screenWidth / 2) - (x.length() / 2.0)) - (91 - x.length() / 2.0)), this.screenHeight - 33, 16777215);
-					client.font.drawShadow(poseStack, y, (int) ((this.screenWidth / 2) - (y.length() / 2.0)) - (y.length() * 2), this.screenHeight - 33, 16777215);
-					client.font.drawShadow(poseStack, z, (((this.screenWidth / 2) - z.length()) + (90 - z.length() * 4)), this.screenHeight - 33, 16777215);
-					poseStack.popPose();
+					if (client.gameMode.getPlayerMode() == GameType.CREATIVE || client.gameMode.getPlayerMode() == GameType.SPECTATOR && !ModConfig.hideInSpectator) {
+						poseStack.pushPose();
+						assert client.player != null;
+						String x = "X: " + (int) client.player.getX();
+						String y = "Y: " + (int) client.player.getY();
+						String z = "Z: " + (int) client.player.getZ();
+						client.font.drawShadow(poseStack, x, (int) (((this.screenWidth / 2) - (x.length() / 2.0)) - (91 - x.length() / 2.0)), this.screenHeight - 33, 16777215);
+						client.font.drawShadow(poseStack, y, (int) ((this.screenWidth / 2) - (y.length() / 2.0)) - (y.length() * 2), this.screenHeight - 33, 16777215);
+						client.font.drawShadow(poseStack, z, (((this.screenWidth / 2) - z.length()) + (90 - z.length() * 4)), this.screenHeight - 33, 16777215);
+						poseStack.popPose();
+					}
+					else {
+						int yOffset = 60;
+						assert client.player != null;
+						if (client.player.hasEffect(MobEffects.HEALTH_BOOST) || client.player.hasEffect(MobEffects.ABSORPTION)) yOffset = 65;
+						poseStack.pushPose();
+						assert client.player != null;
+						String x = "X: " + (int) client.player.getX();
+						String y = "Y: " + (int) client.player.getY();
+						String z = "Z: " + (int) client.player.getZ();
+						client.font.drawShadow(poseStack, x, (int) (((this.screenWidth / 2) - (x.length() / 2.0)) - (91 - x.length() / 2.0)), this.screenHeight - yOffset, 16777215);
+						client.font.drawShadow(poseStack, y, (int) ((this.screenWidth / 2) - (y.length() / 2.0)) - (y.length() * 2), this.screenHeight - yOffset, 16777215);
+						client.font.drawShadow(poseStack, z, (((this.screenWidth / 2) - z.length()) + (90 - z.length() * 4)), this.screenHeight - yOffset, 16777215);
+						poseStack.popPose();
+					}
 				}
 			}
 		}
